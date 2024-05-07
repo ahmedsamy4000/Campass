51ad981604d15a7b149819b2ace8ce9579f3a0fe
ReemHatem <hatemreem37@gmail.com>
2024-05-07 21:04:26 +0300

    reem

--- src/components/Feedbacks/Feedbacks.jsx
+++ src/components/Feedbacks/Feedbacks.jsx
@@ -1,19 +1,26 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { feedBacksAction } from '../../redux/slices/FeedbacksSlice';
+import classes from '../../Styles/Feedbacks.module.css';
 
 const Feedbacks = () => {
-    const feedbacks = useSelector(state=>state.feedbacks.feedbacks)
+    const feedbacks = useSelector(state => state.feedbacks.feedbacks)
     const dispatch = useDispatch()
-    useEffect(()=>{
+    useEffect(() => {
         dispatch(feedBacksAction())
-    },[dispatch])
+    }, [dispatch])
     console.log(feedbacks)
     return (
-        <div>
-            
+        <div className={classes.div1}>
+            {feedbacks.map((feedback, index) => (
+                <div key={index} className={classes.div2}>
+                    <p>{feedback.message}</p>
+                </div>
+            ))}
         </div>
     );
 }
 
 export default Feedbacks;
+
+
