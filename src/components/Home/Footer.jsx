f7c35fbd98ea900e2743c21a4e28ef001f8146c8
ReemHatem <hatemreem37@gmail.com>
2024-05-08 02:17:03 +0300

    reem

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,53 +1,82 @@
 import React from 'react';
-import classes from '../../Styles/Header.module.css'
+import classes from '../../Styles/Footer.module.css'
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <footer className={classes.footer}>
-                    <div className={classes.row}>
-                        {/* <div className={classes.col6}>
-                            <p>
-                                <i className="fa fa-phone" aria-hidden="true" /> +44 (0)123 456 789
-                            </p>
-                            <p>
-                                <i className="fa fa-envelope" aria-hidden="true" />{" "}
-                                info@landingpage.com
-                            </p>
-                        </div> */}
-                        <div className={classes.col4} style={{ textAlign: "left" }}>
-                            <h3>Campass</h3>
-                            <Link>
-                                Contact Us
-                            </Link>
-                            <br></br>
-                            <Link>
-                                About
-                            </Link>
-                        </div>
-                        <div className={classes.col4} style={{ textAlign: "left" }}>
-                            <Link>
-                                Programs
-                            </Link>
-                            <br></br>
-                            <Link>
-                                Countries
-                            </Link>
-                        </div>
-                        <div className={classes.col4} style={{ textAlign: "left" }}>
-                            <Link>
-                                Profile
-                            </Link>
-                        </div>
-                    </div>
-                    {/* <hr />
-                    <div className={classes.row}>
-                        <div className={classes.col12}>
-                            © 2017 Brand - <a href="#">Facebook</a> - <a href="#">Twitter</a>
-                        </div>
-                    </div> */}
-                </footer>
+        <>
+            <div className={classes.scallopup}></div>
+            <div className={classes.colourblock}>
+                <h1>Scallop Pointing Up.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+            </div>
+        </>
     );
 }
 
 export default Footer;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import classes from '../../Styles/Header.module.css'
+// import { Link } from 'react-router-dom';
+
+// const Footer = () => {
+//     return (
+//         <footer className={classes.footer}>
+//                     <div className={classes.row}>
+//                         {/* <div className={classes.col6}>
+//                             <p>
+//                                 <i className="fa fa-phone" aria-hidden="true" /> +44 (0)123 456 789
+//                             </p>
+//                             <p>
+//                                 <i className="fa fa-envelope" aria-hidden="true" />{" "}
+//                                 info@landingpage.com
+//                             </p>
+//                         </div> */}
+//                         <div className={classes.col4} style={{ textAlign: "left" }}>
+//                             <h3>Campass</h3>
+//                             <Link>
+//                                 Contact Us
+//                             </Link>
+//                             <br></br>
+//                             <Link>
+//                                 About
+//                             </Link>
+//                         </div>
+//                         <div className={classes.col4} style={{ textAlign: "left" }}>
+//                             <Link>
+//                                 Programs
+//                             </Link>
+//                             <br></br>
+//                             <Link>
+//                                 Countries
+//                             </Link>
+//                         </div>
+//                         <div className={classes.col4} style={{ textAlign: "left" }}>
+//                             <Link>
+//                                 Profile
+//                             </Link>
+//                         </div>
+//                     </div>
+//                     {/* <hr />
+//                     <div className={classes.row}>
+//                         <div className={classes.col12}>
+//                             © 2017 Brand - <a href="#">Facebook</a> - <a href="#">Twitter</a>
+//                         </div>
+//                     </div> */}
+//                 </footer>
+//     );
+// }
+
+// export default Footer;
--- src/components/Home/Header.jsx
+++ src/components/Home/Header.jsx
@@ -1,117 +1,57 @@
-import React from 'react';
-import classes from '../../Styles/Header.module.css'
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
 import { Link } from 'react-router-dom';
-import Body from './Body';
-import Intro from './intro';
-import { useDispatch, useSelector } from 'react-redux';
-import { changeAuth } from '../../redux/slices/authSlice';
-
+import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 const Header = () => {
-    const isAuth = useSelector(state => state.isAuth.isAuth);
-    const dispatch = useDispatch();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
-            <>
-            {/* <ScrollMenuComponent></ScrollMenuComponent>
-            <MobileMenuComponent></MobileMenuComponent> */}
-                <nav className={classes.nav} id="menu">
-                    <div className={classes.wrap}>
-                        <div className={classes.brand}>
-                            <Link to="/">Campass</Link>
-                        </div>
-                        <button id="mobile-btn" className={classes.hamburgerBtn}>
-                            <span className={classes.hamburgerLine} />
-                            <span className={classes.hamburgerLine} />
-                            <span className={classes.hamburgerLine} />
-                        </button>
-                        <ul className={classes.topMenu} id="top-menu">
-                            <li>
-                                {/* <Link to="#">
-                                    <i className="fa fa-home" aria-hidden="true" />
-                                </Link> */}
-                            </li>
-                            <li>
-                                {(isAuth && localStorage.getItem('type') === 'Company') ? 
-                                <Link to="/company/programs">Programs</Link>:
-                                <Link to="/programs">Programs</Link>}
-                            </li>
-                            <li>
-                            {isAuth && localStorage.getItem('type') === 'Admin' && (<Link to="/analysis">Analysis</Link>)}
-                            </li>
-                            <li>
-                                <Link to="/countries">Countries</Link>
-                            </li>
-                            <li>
-                                <Link to="/habitations">Habitations</Link>
-                            </li>
-                            <li>
-                                <Link to="/contact">Contact Us</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                               { !isAuth? 
-                               <Link to="/signin" className={`${classes.button} ${classes.flower}`}>Login</Link>:
-                               <Link to="/" className={`${classes.button} ${classes.flower}`} onClick={()=>{
-                                localStorage.removeItem('email'); 
-                                localStorage.removeItem('type');
-                                dispatch(changeAuth(false));
-                            }}>Logout</Link>
-                            }
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-                {/* <Body></Body> */}
-                {/* <Intro></Intro> */}
-                {/* <footer className={classes.footer}>
-                    <div className={classes.row}>
-                        <div className={classes.col6}>
-                            <p>
-                                <i className="fa fa-phone" aria-hidden="true" /> +44 (0)123 456 789
-                            </p>
-                            <p>
-                                <i className="fa fa-envelope" aria-hidden="true" />{" "}
-                                info@landingpage.com
-                            </p>
-                        </div>
-                        <div className={classes.col4} style={{ textAlign: "left" }}>
-                            <h3>Campass</h3>
-                            <Link>
-                                Contact Us
-                            </Link>
-                            <br></br>
-                            <Link>
-                                About
-                            </Link>
-                        </div>
-                        <div className={classes.col4} style={{ textAlign: "left" }}>
-                            <Link>
-                                Programs
-                            </Link>
-                            <br></br>
-                            <Link>
-                                Countries
-                            </Link>
-                        </div>
-                        <div className={classes.col4} style={{ textAlign: "left" }}>
-                            <Link>
-                                Profile
-                            </Link>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className={classes.row}>
-                        <div className={classes.col12}>
-                            © 2017 Brand - <a href="#">Facebook</a> - <a href="#">Twitter</a>
-                        </div>
-                    </div>
-                </footer> */}
-            </>
-        </div>
+
+        <Box sx={{ width: '100%', position: "absolute" }}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider' }} display={'flex'} justifyContent={'end'}>
+                <Tabs value={value} onChange={handleChange} aria-label="basic tabs example"
+                    sx={{ "& .MuiTabs-indicator": { backgroundColor: '#DFD0B8' } }} >
+                    <Tab label="Home" component={Link} to="/" sx={{ color: "white", '&.Mui-selected': { color: '#DFD0B8' }, marginRight: "230px" }} />
+                    <Tab label="Contact Us" component={Link} to="/contact" sx={{ color: "white", '&.Mui-selected': { color: '#DFD0B8' }, marginRight: "230px" }} />
+                    <Tab label="About" component={Link} to="/about" sx={{ color: "white", marginRight: "230px", '&.Mui-selected': { color: '#DFD0B8' } }} />
+                    <Tab label={<FavoriteOutlinedIcon></FavoriteOutlinedIcon>} component={Link} to="/favorite" sx={{ color: "white", '&.Mui-selected': { color: '#DFD0B8' }, }} />
+                    <Tab label={<AccountCircleOutlinedIcon></AccountCircleOutlinedIcon>} component={Link} to="/signin" sx={{ color: "white", '&.Mui-selected': { color: '#DFD0B8' } }} />
+                </Tabs>
+
+            </Box>
+        </Box>
+
     );
 }
 
 export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
