2ee0c299fc58c4142615e983cb1ce8c0df7b5d40
ReemHatem <hatemreem37@gmail.com>
2024-05-09 06:12:54 +0300

    reem

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import classes from '../../Styles/Footer.module.css'
 import { Link } from 'react-router-dom';
+import { Stack } from '@mui/material';
+import { useSelector } from 'react-redux';
 
 const Footer = () => {
+    const isAuth = useSelector(state=>state.isAuth.isAuth)
+    const type = localStorage.getItem('type')
     return (
         <>
             <div className={classes.scallopup}></div>
-            <div className={classes.colourblock}>
-                <h1>Campass</h1>
-                <p><Link to='/programs'>Programs</Link></p>
-                <p><Link to='/countries'>Countries</Link></p>
-                <p><Link to='/company/programs'>Campany</Link></p>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
+            <div className={classes.colourblock} style={{ width: "100%" }}>
+
+                <Stack direction='row' spacing={10} justifyContent={'space-between'} width={'100%'}>
+                    <h1 className={classes.h1}>Campass</h1>
+                    <div>
+                        <h3>Quick Links</h3>
+                        <p><Link className={classes.p} to='/countries'>Countries</Link></p>
+                        <p><Link className={classes.p} to='/habitations'>Habitations</Link></p>
+                        <p><Link className={classes.p} to='/programs'>Programs</Link></p>
+                        <p><Link className={classes.p} to='/about'>About</Link></p>
+                        {!isAuth&&<p><Link className={classes.p} to='/signin'>Reservations</Link></p>}
+                        {isAuth&&type=='Traveller'&&<p><Link className={classes.p} to='/userReservations'>Reservations</Link></p>}
+                    </div>
+                </Stack>
+
             </div>
         </>
     );
@@ -19,67 +32,5 @@ const Footer = () => {
 
 export default Footer;
 
-
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import classes from '../../Styles/Header.module.css'
-// import { Link } from 'react-router-dom';
-
-// const Footer = () => {
-//     return (
-//         <footer className={classes.footer}>
-//                     <div className={classes.row}>
-//                         {/* <div className={classes.col6}>
-//                             <p>
-//                                 <i className="fa fa-phone" aria-hidden="true" /> +44 (0)123 456 789
-//                             </p>
-//                             <p>
-//                                 <i className="fa fa-envelope" aria-hidden="true" />{" "}
-//                                 info@landingpage.com
-//                             </p>
-//                         </div> */}
-//                         <div className={classes.col4} style={{ textAlign: "left" }}>
-//                             <h3>Campass</h3>
-//                             <Link>
-//                                 Contact Us
-//                             </Link>
-//                             <br></br>
-//                             <Link>
-//                                 About
-//                             </Link>
-//                         </div>
-//                         <div className={classes.col4} style={{ textAlign: "left" }}>
-//                             <Link>
-//                                 Programs
-//                             </Link>
-//                             <br></br>
-//                             <Link>
-//                                 Countries
-//                             </Link>
-//                         </div>
-//                         <div className={classes.col4} style={{ textAlign: "left" }}>
-//                             <Link>
-//                                 Profile
-//                             </Link>
-//                         </div>
-//                     </div>
-//                     {/* <hr />
-//                     <div className={classes.row}>
-//                         <div className={classes.col12}>
-//                             © 2017 Brand - <a href="#">Facebook</a> - <a href="#">Twitter</a>
-//                         </div>
-//                     </div> */}
-//                 </footer>
-//     );
-// }
-
-// export default Footer;
+//guest sign in
+//is auth traveller reservations
